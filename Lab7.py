298b3fd25e2deb492f9b9003eceeedca7525fa88
Butteryfinger <38005380+Butteryfinger@users.noreply.github.com>
2018-05-31 23:02:47 +0200

    Add files via upload

--- Lab7.py
+++ Lab7.py
@@ -1,3 +1,5 @@
+# Base done, kolla för eventuella krav
+
 def lexer(inp):
     inp = list(inp)
     while True:
@@ -8,6 +10,15 @@ def lexer(inp):
                 inp = inp[:t1] + inp[t2+1:]
         else:
             break
+    startpar = 0
+    endpar = 0
+    for a in inp:
+        if a == "(":
+            startpar += 1
+        elif a == ")":
+            endpar += 1
+    if not startpar == endpar:
+        raise ValueError("Uneven amount of parenthesis")
     return inp
 
 class Tree:
@@ -25,12 +36,62 @@ class Tree:
     def addleaves(self):
         self.__leaves += 1
 
-    def addheight(self):
-        self.__height += 1
+    def mostheight(self, cal):
+        if cal > self.__height:
+            self.__height = cal
 
-d = "(a, ( b,[sfagdsfdsfs] c )));"
-print(lexer(d))
+    def deletet(self):
+        self.__leaves = 0
+        self.__height = 0
 
-def parse_newick(terminals):
+def parse_newick(terminal):
+
+    if ";" not in terminal:
+        raise ValueError("No end in sight")
+    while " " in terminal:
+        terminal.remove(" ")
+    while "," in terminal:
+        terminal.remove(",")
+    present = Tree()
+    cur, *other = terminal
+    if other == [";"]:
+       present.mostheight(1)
+       present.addleaves()
+    elif cur == "(":
+       parse_main(terminal, 1, present,0)
+    else: # Om nånting gick fel
+       raise NameError("Något gick fel")
+    return present
+       
+
+def parse_main(terminal, high, cla, leaf):
+    if terminal == [";"]:
+        return
+    cur, *other = terminal
     if cur == "(":
-        
+        parse_main(other, high + 1, cla, 0)
+    elif cur == ")":
+        cla.mostheight(high)
+        parse_main(other, high - 1, cla, 0)
+    else:
+        if leaf == 2:
+            raise ValueError("Tree can't have 3 leaf silly!")
+        cla.addleaves()
+        parse_main(other, high, cla, leaf + 1)   
+
+
+term1 = "(a, (b, (c, (d, e))));"
+term2 = "((a,b), (c, d));"
+term3 = "((a,b)[SECRET!],(b,c));" # Bracket killing
+term4 = "(a,);" # Tom löv
+term5 = "(((()));" # Ojämnt parenteser
+term6 = "(1,2)" # Slut tecken utelagt
+term7 = "(1,2,3);" # Triple leaf power
+
+t = parse_newick(lexer(term3))
+print(t.n_leaves())
+print(t.height())
+
+
+
+
--- Lab8.py
+++ Lab8.py
@@ -1,3 +1,7 @@
+from multiprocessing import Process
+import time
+#import matplotlib.pyplot as plt
+
 def Sieve(n):
     Prim = list(range(1,n+1))
     Comp = []
@@ -14,10 +18,62 @@ def Sieve(n):
                Prim.remove(e)
        Pointer += 1
     Prim = list(set(Prim)-set(Comp))
-    Comp.sort()
-    return Prim, Comp
+    Comp = sorted(Comp)
+    return Prim
 
-# Test med 10 000 element ger drygt 0.255 sek med unittest
+# Test med 10 000 element ger drygt 0.205 sek med time modulen
 
+if __name__ == '__main__':
+    experiments = list(range(2,6))
+    thread = []
+    serial = []
 
+    
+    for it in experiments:
+        jobs = []
+        now = time.clock()
+        S = Process(target = Sieve, args = (10**it,))
+        jobs.append(S)
+        S.start()
+        S.join()
+        thread.append(time.clock()- now)
+    print(thread)
+
+    """
+    for it in experiments:
+        now = time.clock()
+        Sieve(10**it)
+        serial.append(time.clock() - now)
+    print(serial)
+    
+    
+    now = time.clock()
+    Sieve(10**5)
+    print(time.clock() - now)
+    
+    jobs = []
+    now = time.clock()
+    while True:
+        S = Process(target = Sieve, args = (10**5,))
+        
+        S.start()
+        #S.join()
+        break
+    print(time.clock()- now)
+    """
+
+    #plt.plot(thread, experiments)
+    #plt.savefig("Threaded")
+
+    #plt.plot(serial, experiments)
+    #plt.savefig("Serial")
+        
+
+# Intressant att vid låga antal, så märks nästan ingen förbättring
+
+# Ser om skolans dator är bättre.
+
+# Misc frågor om vissa ämne
+# Vid heaps, kan det finnas "tomma" löv alltså gren 3 och 5 har löv men inga andra vid samma nivå
+# Vilka graph metoder finns det annars
 
